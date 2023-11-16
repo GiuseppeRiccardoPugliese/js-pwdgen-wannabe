01db1f80e387c0989ea26ad8716e44e2cbb0f9bf
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-11-16 16:55:17 +0100

    Finito

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+/* Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23. */
+
+
+//Chiedi di inserire all'utente il suo NOME
+const userName = (prompt("Per favore, inserisci il tuo nome"))
+
+//Chiedi di inserire all'utente il suo COGNOME
+const userSurname = (prompt("Per favore, inserisci il tuo cognome"))
+
+//Chiedi di inserire all'utente il suo COLORE PREFERITO
+const userPrefColor = (prompt("Per favore, inserisci il tuo colore preferito"))
+
+//Output basato su input dell'utente
+
+//document.getElementById("output").innerHTML = `${userName} ${userSurname} ${userPrefColor} 23`
+document.getElementById("output").innerHTML = userName + userSurname + userPrefColor + "23";
+
